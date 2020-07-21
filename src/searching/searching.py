354304391794b8aeaaa1b830263dd7bd426f3de1
JimKing100 <JimKing100@users.noreply.github.com>
2020-07-21 08:47:28 -0700

    searching

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,21 @@
 def linear_search(arr, target):
-    # Your code here
-
-
-    return -1   # not found
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i
+    return -1
 
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
-
-    # Your code here
-
-
-    return -1  # not found
+    low_point = 0
+    high_point = len(arr) - 1
+    while low_point <= high_point:
+        mid_point = (low_point + high_point) // 2
+        guess = arr[mid_point]
+        if guess == target:
+            return mid_point
+        if guess > target:
+            high_point = mid_point - 1
+        else:
+            low_point = mid_point + 1
+    return -1
